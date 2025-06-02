6bd6de36b65416736542400edecc137251c2024a
olivierfalahi <olivier.falahi@gmail.com>
2025-06-02 09:13:16 +0200

    :recycle: Refactor Header component for cleaner and dynamic rendering.
    Simplified login state handling by directly fetching the user. Improved conditional rendering to personalize the header for logged-in users and cleaned up code formatting for consistency.

--- components/header.tsx
+++ components/header.tsx
@@ -1,70 +1,72 @@
 import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 import {cookies} from "next/headers";
 import {User} from "@/app/generated/prisma";
-import {getUser} from "@/lib/actions";
+import {getUser} from "@/lib/userUtils";
 
 const links = [
-  { href: "/demos", label: "Demos" },
-  { href: "/posts", label: "Posts" },
-  { href: "/prisma/coins", label: "MemeCoin" },
+    {href: "/demos", label: "Demos"},
+    {href: "/posts", label: "Posts"},
+    {href: "/prisma/coins", label: "MemeCoin"},
 ];
 
 const loginLinks = [
-    { href: "/login", label: "Login" },
-    { href: "/logout", label: "Logout" },
-    ];
+    {href: "/login", label: "Login"},
+    {href: "/logout", label: "Logout"},
+];
 
 export default async function Header() {
 
-  const cookieStore = await cookies();
-  const isLoggedIn = cookieStore.get('session');
-  if (isLoggedIn) {
-      const user: User = await getUser(isLoggedIn?.value);
-  }
+    const isLoggedIn: User | null = await getUser();
 
-  const loginLink  = isLoggedIn ? loginLinks[1] : loginLinks[0];
+    const loginLink = isLoggedIn ? loginLinks[1] : loginLinks[0];
 
-  return (
-    <header className="bg-white border-b">
-      <div className="flex items-center justify-between p-4">
-        <Link href="/" className="text-xl font-light">
-          NextJS Demo App
-        </Link>
-
-        <NavigationMenu>
-          <NavigationMenuList>
-            {links.map((link) => (
-              <NavigationMenuItem key={link.href}>
-                <NavigationMenuLink
-                  asChild
-                  className={navigationMenuTriggerStyle()}
-                >
-                  <Link href={link.href} passHref>
-                    {link.label}
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            ))}
-                <NavigationMenuItem key={loginLink.href}>
-                  <NavigationMenuLink
-                      asChild
-                      className={navigationMenuTriggerStyle()}
-                  >
-                    <Link href={loginLink.href} passHref>
-                      {loginLink.label}
+    return (
+        <header className="bg-white border-b">
+            <div className="flex items-center justify-between p-4">
+                {!isLoggedIn ?
+                    <Link href="/" className="text-xl font-light">
+                        NextJS Demo App
                     </Link>
-                  </NavigationMenuLink>
-                </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      </div>
-    </header>
-  );
+                    :
+                    <Link href="/" className="text-xl font-light">
+                        Bonjour {isLoggedIn.name} !
+                    </Link>
+                }
+
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        {links.map((link) => (
+                            <NavigationMenuItem key={link.href}>
+                                <NavigationMenuLink
+                                    asChild
+                                    className={navigationMenuTriggerStyle()}
+                                >
+                                    <Link href={link.href} passHref>
+                                        {link.label}
+                                    </Link>
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                        ))}
+                        <NavigationMenuItem key={loginLink.href}>
+                            <NavigationMenuLink
+                                asChild
+                                className={navigationMenuTriggerStyle()}
+                            >
+                                <Link href={loginLink.href} passHref>
+                                    {loginLink.label}
+                                </Link>
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
+        </header>
+    );
 }
